a72cb3181033965b712c25292df186d3b3eaf5a5
wasim Haider55 <wasimhaider14379@gmail.com>
2023-06-26 16:05:23 +0500

    worked on contact components

--- onePortfolio/src/components/Contact.jsx
+++ onePortfolio/src/components/Contact.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useFormik } from 'formik';
+
 
 const Contact = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            phone: "",
+            email: ""
+        }
+    })
     return (
         <section id='contact'
             className='w-full h-140 bg-gradient-to-b from-black to-gray-800  p-4 text-white pt-32'>
@@ -10,19 +19,40 @@ const Contact = () => {
                     <p className=' py-6'>Submit the form below to get in touch with me </p>
                 </div>
                 <div className='flex justify-center items-center'>
-                    <form action="https://getform.io/f/7f3af3e2-4f2b-45bf-9d0a-f3d2aca43886" method='POST' className=' flex flex-col w-full md:w-1/2 '>
-                        <input type="text" name='name' placeholder='Enter Your Name '
+                    <form
+                        action="https://getform.io/f/7f3af3e2-4f2b-45bf-9d0a-f3d2aca43886"
+                        method='POST'
+                        className=' flex flex-col w-full md:w-1/2 '>
+                        <input
+                            type="text"
+                            name='name'
+                            id='name'
+                            placeholder='Enter Your Name '
                             className=' p-2  bg-transparent border-2 rounded-md  text-white focus:outline-none'
                         />
-                        <input type="text" name='name' placeholder='Enter Phone Number'
+                        <input
+                            type="text"
+                            name='phone'
+                            placeholder='Enter Phone Number'
                             className=' mt-4 p-2  bg-transparent border-2 rounded-md  text-white focus:outline-none'
                         />
-                        <input type="text" name='email' placeholder='Enter Your Email '
+                        <input
+                            type="text"
+                            name='email'
+                            id='email'
+                            placeholder='Enter Your Email '
                             className=' my-4 p-2 bg-transparent border-2 rounded-md  text-white focus:outline-none'
                         />
-                        <textarea name="message" rows="10" placeholder='Enter your message here!!!' className='p-2 bg-transparent border-2 rounded-md  text-white focus:outline-none'></textarea>
-                        <button className=' text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3  my-8 
-                      mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
+                        <textarea
+                            name="message"
+                            rows="10"
+                            placeholder='Enter your message here!!!'
+                            className='p-2 bg-transparent border-2 rounded-md  text-white focus:outline-none'>
+                        </textarea>
+                        <button
+                            className=' text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3  my-8 
+                                mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk
+                        </button>
                     </form>
                 </div>
             </div>
